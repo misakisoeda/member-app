227a130e11b0192da15600e6527dfc46d9cffb7b
misakisoeda <misaki.soeda.29@gmail.com>
2023-12-29 11:20:12 +0900

    feat: vueをインストール、微調節

---
+++ vue-project/src/components/Kv.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div>
+    メイン画像はここにおきます
+  </div>
+</template>
---
+++ vue-project/src/views/HomeView.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+  import Kv from '../components/Kv.vue';
+</script>
+
+<template>
+  <main>
+    <div class="container-l" aria-hidden="true"></div>
+    <div class="container-r" aria-hidden="true"></div>
+    <div class="container">
+      <Kv />
+    </div>
+  </main>
+</template>
